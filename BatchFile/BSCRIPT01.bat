6c714be4e55101e2dd004bdec96438903c472db0
Eddie Peckley <eddiepecs@gmail.com>
2024-07-12 19:18:19 +0800

    This Batch File Opens Five websites, launches the calculator and notepad, then initiates a system shutdown after a brief delay.

---
+++ BatchFile/BSCRIPT01.bat
@@ -0,0 +1,17 @@
+@echo off
+
+start https://www.google.com
+start https://www.gogoanime3.co
+start https://www.Youtube.com
+start https://h.mangabat.com/mangabat
+start https://www.discord.com
+
+start calc.exe
+
+start notepad.exe
+
+shutdown /s /t 30
+
+echo The system will shut down in 1 minute.
+
+pause
